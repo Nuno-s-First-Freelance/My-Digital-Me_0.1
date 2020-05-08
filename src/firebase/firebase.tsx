40bf7b17052ecff4b56721ab3a4805d937a766b7
Nuno Moreira <nunomoreira16@gmail.com>
2020-05-08 17:30:59 +0200

    cleaning code

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,17 +1,8 @@
 import Firebase from "firebase";
+import { FIREBASE_CONFIG } from "./constants";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC4x90wAoLQl6zT7ku0eyL0LtfW-8k043k",
-  authDomain: "my-digital-me.firebaseapp.com",
-  databaseURL: "https://my-digital-me.firebaseio.com",
-  projectId: "my-digital-me",
-  storageBucket: "my-digital-me.appspot.com",
-  messagingSenderId: "186773389559",
-  appId: "1:186773389559:web:249b96591fd13cc1f1d3f2",
-  measurementId: "G-HEMW8Z3RKE",
-};
-
-const app = Firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = Firebase.initializeApp(FIREBASE_CONFIG);
 
 // Database
 export const database = app.database();
