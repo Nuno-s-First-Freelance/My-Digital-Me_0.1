f50bd534fa12c1c29ad79a0dda3316a7089170f7
funo-s-first-freelance <nuno.s.first.freelance@gmail.com>
2020-05-16 17:13:45 +0200

    add dev tools extension for dev use.

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,12 @@
 import { createStore } from "redux";
 import { ProfileState } from "./ProfileReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const initialState = {
   profileState: ProfileState,
 };
 
-export const store = createStore((initialState, action) => initialState);
+export const store = createStore(
+  (initialState, action) => initialState,
+  composeWithDevTools()
+);
