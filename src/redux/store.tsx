dafc531b1bcca9e33a0729b30ff76ba5e8b34b9d
funo-s-first-freelance <nuno.s.first.freelance@gmail.com>
2020-05-16 17:14:32 +0200

    remove devtools extension

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,8 @@
 import { createStore } from "redux";
 import { ProfileState } from "./ProfileReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 export const initialState = {
   profileState: ProfileState,
 };
 
-export const store = createStore(
-  (initialState, action) => initialState,
-  composeWithDevTools()
-);
+export const store = createStore((initialState, action) => initialState);
